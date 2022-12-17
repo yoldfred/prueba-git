b6aec5532a2877f9f37dc2620bfd63e31e3a5073
yoldfred <yoldfred.suarez@gmail.com>
2022-12-17 13:47:49 -0400

    creado eventos

--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
 const boton = document.getElementById('buttonShow');
 const show = document.getElementById('html');
 
+boton.addEventListener('click',()=>{
+
+});
+window.addEventListener('resize',()=>{
+
+});
