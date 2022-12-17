d0d31571f0b1e1557e3814e72423ac16116198f7
yoldfred <yoldfred.suarez@gmail.com>
2022-12-17 13:51:40 -0400

    aplicando funcion al boton

--- js/app.js
+++ js/app.js
@@ -2,8 +2,9 @@ const boton = document.getElementById('buttonShow');
 const show = document.getElementById('html');
 
 boton.addEventListener('click',()=>{
-
+    show.classList.toggle('backgroundShow');
+    boton.classList.toggle('botonShow')
 });
 window.addEventListener('resize',()=>{
-
+    
 });
